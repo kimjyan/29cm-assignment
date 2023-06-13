771e45145202133539ca9b1d8a8b8b7c5c72bda7
쎄부킴 <ssebukim@baegyeong-won-ui-MacBookAir.local>
2023-06-14 00:27:13 +0900

    add xcode project

---
+++ assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment
+//
+//  Created by 쎄부킴 on 2023/06/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
