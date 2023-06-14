5fcfd8d9e285b658cedfb0f26a10c4d676d84fb4
쎄부킴 <ssebukim@baegyeong-won-ui-MacBookAir.local>
2023-06-15 01:44:04 +0900

    add GitAPI, AuthenicatedUser model

---
+++ assignment/GitAPI.swift
@@ -0,0 +1,42 @@
+//
+//  GitAPI.swift
+//  assignment
+//
+//  Created by 쎄부킴 on 2023/06/15.
+//
+
+import Foundation
+import Moya
+
+enum GitAPI {
+    case authenticatedUser
+}
+
+extension GitAPI: Moya.TargetType {
+    var baseURL: URL {
+        return URL(string: "https://github.com")!
+    }
+    
+    var path: String {
+        switch self {
+        case .authenticatedUser:
+            return "/user"
+        }
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var sampleData: Data {
+        return Data()
+    }
+    
+    var task: Moya.Task {
+        return .requestPlain
+    }
+    
+    var headers: [String : String]? {
+        return ["Accept": "application/json"]
+    }
+}
