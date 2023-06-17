8df1be818c14ee8498de14811f33283c44eb07b6
쎄부킴 <ssebukim@baegyeong-won-ui-MacBookAir.local>
2023-06-17 23:11:53 +0900

    SnapKit 추가

--- assignment/UI/UIViewPreview.swift
+++ assignment/UI/UIViewPreview.swift
@@ -5,4 +5,24 @@
 //  Created by 쎄부킴 on 2023/06/17.
 //
 
-import Foundation
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+struct UIViewPreview<View: UIView>: UIViewRepresentable {
+    let view: View
+
+    init(_ builder: @escaping () -> View) {
+        view = builder()
+    }
+
+    // MARK: - UIViewRepresentable
+
+    func makeUIView(context: Context) -> UIView {
+        return view
+    }
+
+    func updateUIView(_ view: UIView, context: Context) {
+        view.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        view.setContentHuggingPriority(.defaultHigh, for: .vertical)
+    }
+}
+#endif
