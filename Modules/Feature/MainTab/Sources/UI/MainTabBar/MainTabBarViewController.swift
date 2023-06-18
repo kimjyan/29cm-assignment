98a175b173f821038b374a1e97feeceda1a81faf
쎄부킴 <ssebukim@baegyeong-won-ui-MacBookAir.local>
2023-06-18 23:13:04 +0900

    login, logout button bind

--- Modules/Feature/MainTab/Sources/UI/MainTabBar/MainTabBarViewController.swift
+++ Modules/Feature/MainTab/Sources/UI/MainTabBar/MainTabBarViewController.swift
@@ -30,18 +30,19 @@ public final class MainTabBarViewController: UITabBarController {
     
     public init() {
         super.init(nibName: nil, bundle: nil)
-
+        
         let viewControllers = [
             makeGitHubSearchViewController(),
             makeGitHubProfileViewController()
         ]
-
+        
         self.viewControllers = viewControllers
         title = "GitHub"
     }
     
     public override func viewDidLoad() {
         super.viewDidLoad()
+        bind()
     }
     
     public override func viewWillAppear(_ animated: Bool) {
@@ -56,12 +57,36 @@ public final class MainTabBarViewController: UITabBarController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
+    func bind() {
+        loginBarButton.rx
+            .tap
+            .asSignal()
+            .withUnretained(self)
+            .emit(onNext: { owner, _ in
+                owner.authService.authorize()
+                    .subscribe(onNext: { _ in
+                        owner.navigationItem.rightBarButtonItem = owner.logoutBarButton
+                    })
+                    .disposed(by: owner.disposeBag)
+            })
+            .disposed(by: disposeBag)
+        
+        logoutBarButton.rx
+            .tap
+            .asSignal()
+            .emit(onNext: { [weak self] _ in
+                self?.authService.logout()
+                self?.navigationItem.rightBarButtonItem = self?.loginBarButton
+            })
+            .disposed(by: disposeBag)
+    }
+    
     private func makeGitHubSearchViewController() -> UIViewController {
         let reactor = GitHubSearchReactor()
         return GitHubSearchViewController(reactor: reactor)
     }
-
+    
     private func makeGitHubProfileViewController() -> UIViewController {
         let reactor = GitHubProfileReactor()
         return GitHubProfileViewController(reactor: reactor)
