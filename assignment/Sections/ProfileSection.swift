1ba723d1f9d8c0655f22bd91575f358c996dfa74
쎄부킴 <ssebukim@baegyeong-won-ui-MacBookAir.local>
2023-06-18 16:13:50 +0900

    ProfileSection user case 삭제

--- assignment/Sections/ProfileSection.swift
+++ assignment/Sections/ProfileSection.swift
@@ -8,27 +8,23 @@
 import RxDataSources
 
 enum ProfileSection {
-    case user([ProfileSectionItem])
     case starred([ProfileSectionItem])
 }
 
 extension ProfileSection: SectionModelType {
     var items: [ProfileSectionItem] {
         switch self {
-        case .user(let item): return item
         case .starred(let items): return items
         }
     }
     
     init(original: ProfileSection, items: [ProfileSectionItem]) {
         switch original {
-        case .user: self = .user(items)
         case .starred: self = .starred(items)
         }
     }
 }
 
 enum ProfileSectionItem {
-    case user(AuthenicatedUser)
     case starred([RepoItem])
 }
