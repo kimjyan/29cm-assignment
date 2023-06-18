b6d0601353b36c4aa675364c1e3936e107528a4a
쎄부킴 <ssebukim@baegyeong-won-ui-MacBookAir.local>
2023-06-18 22:36:47 +0900

    검색하기 UI 작성

--- Modules/Feature/GitHubSearch/Sources/UI/Search/GitHubSearchViewController.swift
+++ Modules/Feature/GitHubSearch/Sources/UI/Search/GitHubSearchViewController.swift
@@ -14,6 +14,21 @@ public final class GitHubSearchViewController: UIViewController {
     private let disposeBag = DisposeBag()
     private let authService = coreContainer.resolve(AuthServiceType.self)!
     
+    private struct Constants {
+        static let repoCellIdentifier = "SearchRepoCell"
+    }
+    
+    private let searchBar = UISearchBar()
+    
+    private let searchResultCollectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .vertical
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.register(SearchRepoCell.self, forCellWithReuseIdentifier: Constants.repoCellIdentifier)
+        return collectionView
+    }()
+    
     public init() {
         super.init(nibName: nil, bundle: nil)
         title = "List"
@@ -22,6 +37,8 @@ public final class GitHubSearchViewController: UIViewController {
     
     public override func viewDidLoad() {
         super.viewDidLoad()
+        setUpView()
+        setUpConstraints()
     }
     
     public override func viewWillAppear(_ animated: Bool) {
@@ -30,6 +47,25 @@ public final class GitHubSearchViewController: UIViewController {
     
     public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpView() {
+        view.addSubview(searchBar)
+        view.addSubview(searchResultCollectionView)
+    }
+    
+    private func setUpConstraints() {
+        searchBar.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(50)
+        }
+        
+        searchResultCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
     }
     
     @objc private func touchedLogin() {
---
+++ Modules/Feature/GitHubSearch/Sources/UI/Search/View/SearchRepoCell.swift
@@ -0,0 +1,136 @@
+//
+//  SearchRepoCell.swift
+//  GitHubSearch
+//
+//  Created by 쎄부킴 on 2023/06/18.
+//
+
+import UIKit
+import SnapKit
+import Kingfisher
+import CoreView
+import CoreNetwork
+
+final class SearchRepoCell: UICollectionViewCell {
+    private let imageView: UIImageView = {
+        let view = UIImageView()
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    private let ownerNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 15, weight: .regular)
+        label.textColor = .gray29cm
+        return label
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 17, weight: .bold)
+        label.textColor = .black29cm
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 17, weight: .regular)
+        label.textColor = .gray29cm
+        return label
+    }()
+    
+    private let starButton: StarButton = {
+        let button = StarButton()
+        button.isSelected = true
+        return button
+    }()
+    
+    private let starImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(systemName: "star")
+        view.tintColor = .gray29cm
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
+    private let starCountLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.textColor = .gray29cm
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpView()
+        setUpConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpView() {
+        contentView.layer.borderWidth = 1
+        contentView.layer.borderColor = UIColor.black29cm.cgColor
+        contentView.layer.cornerRadius = 20
+        contentView.addSubview(imageView)
+        contentView.addSubview(ownerNameLabel)
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(starButton)
+        contentView.addSubview(descriptionLabel)
+        contentView.addSubview(starImageView)
+        contentView.addSubview(starCountLabel)
+    }
+    
+    private func setUpConstraints() {
+        imageView.snp.makeConstraints { make in
+            make.top.equalTo(16)
+            make.leading.equalTo(16)
+            make.size.equalTo(24)
+        }
+        
+        ownerNameLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(imageView)
+            make.leading.equalTo(imageView.snp.trailing).offset(4)
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(16)
+            make.leading.equalTo(imageView.snp.leading)
+        }
+        
+        starButton.snp.makeConstraints { make in
+            make.top.equalTo(16)
+            make.trailing.equalToSuperview().inset(16)
+            make.size.equalTo(24)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(nameLabel.snp.bottom).offset(16)
+            make.leading.equalTo(imageView.snp.leading)
+            make.trailing.equalToSuperview()
+        }
+        
+        starImageView.snp.makeConstraints { make in
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(16)
+            make.leading.equalTo(imageView.snp.leading)
+        }
+        
+        starCountLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(starImageView)
+            make.leading.equalTo(starImageView.snp.trailing).offset(4)
+        }
+    }
+    
+    func configure(with item: GitHubRepoItem) {
+        if let url = URL(string: item.owner.avatarURL) {
+            imageView.kf.setImage(with: url)
+        }
+        ownerNameLabel.text = item.owner.name
+        nameLabel.text = item.name
+        descriptionLabel.text = item.description
+        starCountLabel.text = String(item.starredCount)
+    }
+}
+
